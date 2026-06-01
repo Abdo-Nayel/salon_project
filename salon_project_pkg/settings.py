c92ec0afdd9f3afc2a0ca553825f560b88c80865
Abdo Nayel <abdonayel58@gmail.com>
2026-06-01 15:22:40 +0300

    N1

--- salon_project_pkg/settings.py
+++ salon_project_pkg/settings.py
@@ -56,16 +56,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'salon_project_pkg.wsgi.application'
 
 # PostgreSQL Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Nayel',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Mar15',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Nayel',
-        'USER': 'postgres',
-        'PASSWORD': 'Mar15',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'LOCATION': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
