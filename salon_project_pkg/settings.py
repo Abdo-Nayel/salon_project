bb443eb102dba591f3d9bd217cc92ef9e940e51b
Abdo Nayel <abdonayel58@gmail.com>
2026-06-09 19:02:23 +0300

    N1

--- salon_project_pkg/settings.py
+++ salon_project_pkg/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'salon_project_pkg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'LOCATION': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Nayel',
+        'USER': 'postgres',
+        'PASSWORD': 'Mar15',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
